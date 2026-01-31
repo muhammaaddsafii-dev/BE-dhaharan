a5a1c4d53f2b78f96e1bf4086f2bb11c372bc192
muhammaaddsafii-dev <muhammaaddsafii@gmail.com>
2026-01-31 13:25:10 +0700

    disable browsableAPIrenderer

--- config/settings.py
+++ config/settings.py
@@ -152,7 +152,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
+        # 'rest_framework.renderers.BrowsableAPIRenderer',
     ],
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
